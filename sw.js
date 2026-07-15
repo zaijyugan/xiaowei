4593d9fdd13f232ccaad46ce8cf672d3e210bfca
zaijyugan <zaijyugan@gmail.com>
2026-07-15 03:02:33 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "xiaowei-v14";
+const CACHE = "xiaowei-v15";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./icon-180.png"];
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
